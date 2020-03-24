16dae58586eebd19b876ebb2df3ee012f1955487
wongxy <xiyao.wong@foxmail.com>
2020-03-24 18:41:37 +0800

    Upload set-mirrors.sh

---
+++ set-mirrors.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+# --------------------------------
+# pip
+# --------------------------------
+command -v pip > /dev/null
+if [ $? == 0 ]; then
+    echo "for pip"
+    if [ ! -d "$HOME/.pip" ]; then
+        mkdir "$HOME/.pip"
+    fi
+    pip_conf="$HOME/.pip/pip.conf"
+    echo "[global]" > $pip_conf
+    echo "index-url=http://mirrors.aliyun.com/pypi/simple" >> $pip_conf
+    echo "/* extra-index-url=http://pypi.douban.com/simple */" >> $pip_conf
+    echo "/*     https://pypi.tuna.tsinghua.edu.cn/simple */" >> $pip_conf
+    echo "/*     http://pypi.mirrors.ustc.edu.cn/simple */" >> $pip_conf
+    echo "[install]" >> $pip_conf
+    echo "trusted-host=pypi.douban.com" >> $pip_conf
+    echo "    mirrors.aliyun.com" >> $pip_conf
+    echo "    pypi.tuna.tsinghua.edu.cn" >> $pip_conf
+    echo "    pypi.mirrors.ustc.edu.cn" >> $pip_conf
+    echo "[freeze]" >> $pip_conf
+    echo "timeout = 10" >> $pip_conf
+fi
+
+
+# --------------------------------
+# go
+# --------------------------------
+command -v go > /dev/null
+if [ $? == 0 ]; then
+    echo "for go proxy"
+    go env -w GO111MODULE=auto
+    go env -w GOPROXY=https://goproxy.io,direct
+fi
+
+
+# --------------------------------
+# npm
+# --------------------------------
+command -v npm > /dev/null
+if [ $? == 0 ]; then
+    echo "for npm"
+    npm config set registry https://registry.npm.taobao.org
+fi
+
+
+# --------------------------------
+# docker
+# --------------------------------
